ef21c7e4af890f0e74dd39e0d1e970c3ab62a24c
Luka Gabric <luka.gabric@gmail.com>
2014-08-17 15:38:48 +0200

    initial files

---
+++ talkingmirror.py
@@ -0,0 +1,64 @@
+import cv2.cv as cv
+import sys
+
+# Parameters for haar detection
+# From the API:
+# The default parameters (scale_factor=2, min_neighbors=3, flags=0) are tuned 
+# for accurate yet slow object detection. For a faster operation on real video 
+# images the settings are: 
+# scale_factor=1.2, min_neighbors=2, flags=CV_HAAR_DO_CANNY_PRUNING, 
+# min_size=<minimum possible face size
+
+min_size = (5,5)
+haar_scale = 1.2
+min_neighbors = 2
+haar_flags = 0
+
+smallwidth = 90
+
+def detect_and_draw(img, faceCascade):
+    gray = cv.CreateImage((img.width,img.height), 8, 1)
+    image_scale = img.width / smallwidth
+
+    small_img = cv.CreateImage((cv.Round(img.width / image_scale), cv.Round (img.height / image_scale)), 8, 1)
+    # gray = cv.CreateImage((img.width,img.height), 8, 1)
+    image_scale = img.width / smallwidth
+    # small_img = cv.CreateImage((cv.Round(img.width / image_scale), cv.Round (img.height / image_scale)), 8, 1)
+
+    # convert color input image to grayscale
+    cv.CvtColor(img, gray, cv.CV_BGR2GRAY)
+
+    # scale input image for faster processing
+    cv.Resize(gray, small_img, cv.CV_INTER_LINEAR)
+
+    cv.EqualizeHist(small_img, small_img)
+
+    faces = cv.HaarDetectObjects(small_img, faceCascade, cv.CreateMemStorage(0), haar_scale, min_neighbors, haar_flags, min_size)
+
+    if faces:
+        for ((x, y, w, h), n) in faces:
+            # the input to cv.HaarDetectObjects was resized, so scale the
+            # bounding box of each face and convert it to two CvPoints
+            pt1 = (int(x * image_scale), int(y * image_scale))
+            pt2 = (int((x + w) * image_scale), int((y + h) * image_scale))
+            cv.Rectangle(img, pt1, pt2, cv.RGB(255, 0, 0), 3, 8, 0)
+            print "Face at: ", pt1[0], ",", pt2[0], "\t", pt1[1], ",", pt2[1]
+
+    cv.ShowImage('Camera', img)
+
+if __name__ == "__main__":
+    cv.NamedWindow('Camera', cv.CV_WINDOW_AUTOSIZE)
+    capture = cv.CreateCameraCapture(0) # assume we want first device
+    cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 320)
+    cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
+    faceCascade = cv.Load("face.xml")
+
+    if not capture:
+        print "Error opening capture device"
+        sys.exit(1)
+
+    while True:
+        frame=cv.QueryFrame(capture)
+        detect_and_draw(frame, faceCascade)
+
+        # time.sleep(0.1)
