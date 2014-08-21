de0b539c5e7e4a0d0d71289842e5261c787cf921
Luka Gabric <luka.gabric@gmail.com>
2014-08-21 17:14:18 +0200

    Playback

--- talkingmirror.py
+++ talkingmirror.py
@@ -109,9 +109,9 @@ if __name__ == "__main__":
                 text2 = None
                 textToSpeech = (t.statuses.user_timeline(screen_name="pontifex")[0]["text"]).encode("ascii", "ignore")
 
-                if len(textToSpeech) > 80:
-                    text1 = textToSpeech[:80]
-                    text2 = textToSpeech[80:]
+                if len(textToSpeech) > 98:
+                    text1 = textToSpeech[:98]
+                    text2 = textToSpeech[98:]
                 else:
                     text1 = textToSpeech
 
