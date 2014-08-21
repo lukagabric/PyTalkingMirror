77c1079160a137df2492c1427347090572f2391f
Luka Gabric <luka.gabric@gmail.com>
2014-08-21 23:07:39 +0200

    Playback

--- talkingmirror.py
+++ talkingmirror.py
@@ -105,12 +105,12 @@ if __name__ == "__main__":
         if foundFace and time.time() - lastPlaybackTime > 5:
             text, name = get_random_tweet()
             text = "The operator module exports a set of efficient functions corresponding "
-            + "to the intrinsic operators of Python. For example, operator.add(x, y) is equivalent"
-            + "to the expression x+y. The function names are those used for special class methods; "
-            + "variants without leading and trailing __ are also provided for convenience. The functions "
-            + "fall into categories that perform object comparisons, logical operations, mathematical operations"
-            + "and sequence operations. The object comparison functions are useful for all objects, and are named"
-            + "after the rich comparison operators they support:"
+            text += "to the intrinsic operators of Python. For example, operator.add(x, y) is equivalent"
+            text += "to the expression x+y. The function names are those used for special class methods; "
+            text += "variants without leading and trailing __ are also provided for convenience. The functions "
+            text += "fall into categories that perform object comparisons, logical operations, mathematical operations"
+            text += "and sequence operations. The object comparison functions are useful for all objects, and are named"
+            text += "after the rich comparison operators they support:"
 
             speech_lines = []
 
